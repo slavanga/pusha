d19fc32e6e374853568c0c2ff585341768a26acc
slavanga <lavanga.sergio@gmail.com>
2017-07-23 18:10:17 +0200

    Indent using spaces

--- gulpfile.js
+++ gulpfile.js
@@ -3,51 +3,51 @@ var $ = require('gulp-load-plugins')();
 var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var config = {
-	'src': './',
-	'dest': 'dist/',
-	'minify': true
+  'src': './',
+  'dest': 'dist/',
+  'minify': true
 };
 
 
 // HTML
 gulp.task('html', function() {
-	return gulp.src('*.html')
-		.pipe(browserSync.stream());
+  return gulp.src('*.html')
+    .pipe(browserSync.stream());
 });
 
 // Compile and autoprefix stylesheets
 gulp.task('styles', function() {
-	return gulp.src(config.src + 'scss/*.scss')
-		.pipe($.sass({
-			precision: 8,
-			outputStyle: 'expanded'
-		}).on('error', function(error) {
-			$.util.log($.util.colors.red(error.message));
-			this.emit('end');
-		}))
-		.pipe($.postcss([
-			autoprefixer()
-		]))
-		.pipe(gulp.dest(config.dest + 'css'))
-		.pipe(browserSync.stream())
-		.pipe($.cleanCss({compatibility: 'ie9'}))
-		.pipe($.rename({suffix: '.min'}))
-		.pipe($.if(config.minify, gulp.dest(config.dest + 'css')))
-		.pipe(browserSync.stream());
+  return gulp.src(config.src + 'scss/*.scss')
+    .pipe($.sass({
+      precision: 8,
+      outputStyle: 'expanded'
+    }).on('error', function(error) {
+      $.util.log($.util.colors.red(error.message));
+      this.emit('end');
+    }))
+    .pipe($.postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest(config.dest + 'css'))
+    .pipe(browserSync.stream())
+    .pipe($.cleanCss({compatibility: 'ie9'}))
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.if(config.minify, gulp.dest(config.dest + 'css')))
+    .pipe(browserSync.stream());
 });
 
 // Compile javascript
 gulp.task('scripts', function() {
-	return gulp.src(config.src + 'js/*.js')
-		.pipe(gulp.dest(config.dest + 'js'))
-		.pipe(browserSync.stream())
-		.pipe($.uglify().on('error', function(error) {
-			$.util.log($.util.colors.red(error.message));
-			this.emit('end');
-		}))
-		.pipe($.rename({suffix: '.min'}))
-		.pipe($.if(config.minify, gulp.dest(config.dest + 'js')))
-		.pipe(browserSync.stream());
+  return gulp.src(config.src + 'js/*.js')
+    .pipe(gulp.dest(config.dest + 'js'))
+    .pipe(browserSync.stream())
+    .pipe($.uglify().on('error', function(error) {
+      $.util.log($.util.colors.red(error.message));
+      this.emit('end');
+    }))
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.if(config.minify, gulp.dest(config.dest + 'js')))
+    .pipe(browserSync.stream());
 });
 
 // Build production files
@@ -55,22 +55,22 @@ gulp.task('build', ['html', 'styles', 'scripts']);
 
 // Serve compiled files
 gulp.task('serve', ['build'], function() {
-	browserSync.init({
-		server: true,
-		notify: false,
-		snippetOptions: {
-			rule: {
-				match: /<\/body>/i
-			}
-		}
-	});
+  browserSync.init({
+    server: true,
+    notify: false,
+    snippetOptions: {
+      rule: {
+        match: /<\/body>/i
+      }
+    }
+  });
 });
 
 // Watch files for changes
 gulp.task('watch', function() {
-	gulp.watch(config.src + '*.html', ['html']);
-	gulp.watch(config.src + 'scss/**/*.scss', ['styles']);
-	gulp.watch(config.src + 'js/**/*.js', ['scripts']);
+  gulp.watch(config.src + '*.html', ['html']);
+  gulp.watch(config.src + 'scss/**/*.scss', ['styles']);
+  gulp.watch(config.src + 'js/**/*.js', ['scripts']);
 });
 
 // Run all tasks
