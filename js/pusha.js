6c77674c3e888ca7bdd7ef5d3cf9bc2242d804a1
slavanga <lavanga.sergio@gmail.com>
2019-03-08 21:15:05 +0100

    Update module structure

--- js/pusha.js
+++ js/pusha.js
@@ -2,13 +2,13 @@
 
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require, exports, module);
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
   } else {
     root.Pusha = factory();
   }
-}(this, function(require, exports, module) {
+}(this, function() {
   'use strict';
 
   var supportsPassive = false;
